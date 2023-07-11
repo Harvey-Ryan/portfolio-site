7e866ad323ee51daa1159757a38c5bc04351669e
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-07-11 10:38:00 -0500

    Smooth balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -15,10 +15,10 @@ const Ball = (props) => {
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
+      <ambientLight intensity={0.2} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
+        <icosahedronGeometry args={[1, 5]} />
         <meshStandardMaterial
           color='#fff8eb'
           polygonOffset
